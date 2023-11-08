356dffe2f14fe84a2b2e538ebcb56a5a2ee486f6
jamiefang17 <jamiefang17@gmail.com>
2023-11-08 15:01:25 -0800

    Add getbalance to main.java

--- Bank.java
+++ Bank.java
@@ -2,22 +2,61 @@
  * Bank
  */
 
- enum AccountType {SAVINGS, CHECKING}
+ enum AccountTypeEnum { SAVINGS, CHECKING }
+
 public class Bank {
+    // this balance will track the overall balance stored at the bank
+    private static double BankBalance;
     private double Balance;
+    AccountTypeEnum AccountType;
+    String AuthorizedUser;
+
+    public Bank() {
+        AccountType = AccountTypeEnum.CHECKING;
+        AuthorizedUser= "";
+    }
+
+    public Bank(String incAuthorizedUser) {
+        AccountType = AccountTypeEnum.CHECKING;
+        this.AuthorizedUser = incAuthorizedUser;
+    }
+    
+    public Bank(String incAuthorizedUser, double balance) {
+        AccountType = AccountTypeEnum.CHECKING;
+        this.AuthorizedUser = incAuthorizedUser;
+        BankBalance += balance;
+        Balance = balance;
+    }
 
     public double GetBalance(){
-        return Balance;  
-}
-
-    public double Deposit(double amount){
-        Balance += amount;
         return Balance;
     }
 
-    public double Withdrawl(double amount){
-        Balance -= amount;
-        return Balance;
+    public double GetBankBalance() {
+        return BankBalance;
+    }
 
+    public double Deposit(double amount){
+        BankBalance += amount;
+        return Balance += amount;
+    }
+
+    public double Withdrawal(double amount){
+        double updatedBalance = Balance - amount;
+        double rtnBalance = 0;
+
+        if(updatedBalance >=0){
+            Balance -= amount;
+            BankBalance -= amount;
+            rtnBalance = updatedBalance;
+        } else {
+            double zeroBalance = 0;
+            Balance = zeroBalance;
+            BankBalance -= Balance;
+            rtnBalance = zeroBalance;
+        }
+
+        //System.out.println(zeroBalance);
+        return rtnBalance;
     }
 }
--- Main.java
+++ Main.java
@@ -1,13 +1,30 @@
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {  //static means global set
-        Bank bank1 = new Bank(); //new means creating a new instance variable
-        bank1.Deposit(500);
-        bank1.Deposit(100);
-        double currentBalance = bank1.GetBalance();//becuz balance is private,
-        System.out.println(currentBalance); 
-        bank1.Withdrawl(5);
-        double balanceAfterWithdrawl = bank1.GetBalance();
-        System.out.println(balanceAfterWithdrawl);
+    public static void main(String[] args) {
+        Bank bank1 = new Bank("Jue", 900);
+        System.out.println(bank1.GetBalance());
+        System.out.println(bank1.GetBankBalance());
+        Bank bank2 = new Bank("Dexter", 500);
+        System.out.println(bank2.GetBalance());
+        System.out.println(bank2.GetBankBalance());
+        Bank bank3 = new Bank("Cici", 1000);
+        System.out.println(bank3.GetBalance());
+        System.out.println(bank3.GetBankBalance());
+
+
+        System.out.println(bank1.GetBankBalance());
+        // System.out.println(bank1.AuthorizedUser);
+        // System.out.println(bank1.GetBalance());
+        // System.out.println(bank1.AccountType);
+        // bank1.Deposit(500);
+        // bank1.Deposit(50);
+        // bank1.Deposit(10);
+        // double currentBalance = bank1.GetBalance();
+        // System.out.println(currentBalance);
+        // bank1.Withdrawal(5);
+        // currentBalance = bank1.GetBalance();
+        // System.out.println(currentBalance);
+        // bank1.Balance = 1000000000;
     }
-    
 }
